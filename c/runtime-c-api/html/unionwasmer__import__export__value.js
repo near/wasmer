45d2fe2c5eea6d82140b18354c86e9cfa445e99b
ailisp <ailisp@users.noreply.github.com>
2021-03-10 17:33:31 +0000

    deploy: c5c881e8be2372a41846b4a6840bd4790922cb6a

--- c/runtime-c-api/html/unionwasmer__import__export__value.js
+++
@@ -1,7 +0,0 @@
-var unionwasmer__import__export__value =
-[
-    [ "func", "unionwasmer__import__export__value.html#afe2a1974c914b4a0a1b9dc3ae0b841a3", null ],
-    [ "global", "unionwasmer__import__export__value.html#a9da089173769c619d82cfa7c53a56b30", null ],
-    [ "memory", "unionwasmer__import__export__value.html#a0612ef32a07951db90fd2b68fee83a2e", null ],
-    [ "table", "unionwasmer__import__export__value.html#af9832aaae78ddf30fb617d50b14f1ad4", null ]
-];
---
+++ c/structwasmer__limits__t.js
@@ -0,0 +1,5 @@
+var structwasmer__limits__t =
+[
+    [ "max", "structwasmer__limits__t.html#add178faf96af541cd5e3140ec5e69db1", null ],
+    [ "min", "structwasmer__limits__t.html#af3892688247095f3b0e096eafeb2c467", null ]
+];
