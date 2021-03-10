14d22e684c2c0c92ff754311342cd6a9ab686325
Bo Yao <bo@nearprotocol.com>
2021-03-10 13:38:47 -0800

    bench native artifact se/deser

--- lib/cache/benches/bench_filesystem_cache.rs
+++ lib/cache/benches/bench_filesystem_cache.rs
@@ -7,19 +7,20 @@ use wasmer_compiler_singlepass::Singlepass;
 use wasmer::{Module, Store};
 use wasmer_cache::Cache;
 use wasmer_engine_jit::JIT;
+use wasmer_engine_native::Native;
 
 fn random_key() -> Hash {
     Hash::new(rand::thread_rng().gen::<[u8; 32]>())
 }
 
-pub fn store_cache(c: &mut Criterion) {
+pub fn store_cache_jit(c: &mut Criterion) {
     let tmp_dir = TempDir::new("wasmer-cache-bench").unwrap();
     let mut fs_cache = FileSystemCache::new(tmp_dir.path()).unwrap();
     let compiler = Singlepass::default();
     let store = Store::new(&JIT::new(compiler).engine());
     let module = Module::new(&store, std::fs::read("../../lib/c-api/tests/assets/qjs.wasm").unwrap()).unwrap();
 
-    c.bench_function("store module in filesystem cache", |b| {
+    c.bench_function("store jit module in filesystem cache", |b| {
         b.iter(|| {
             let key = random_key();
             fs_cache.store(key, &module).unwrap()
@@ -27,7 +28,7 @@ pub fn store_cache(c: &mut Criterion) {
     });
 }
 
-pub fn load_cache(c: &mut Criterion) {
+pub fn load_cache_jit(c: &mut Criterion) {
     let tmp_dir = TempDir::new("wasmer-cache-bench").unwrap();
     let mut fs_cache = FileSystemCache::new(tmp_dir.path()).unwrap();
     let compiler = Singlepass::default();
@@ -36,7 +37,38 @@ pub fn load_cache(c: &mut Criterion) {
     let key = Hash::new([0u8; 32]);
     fs_cache.store(key, &module).unwrap();
 
-    c.bench_function("load module in filesystem cache", |b| {
+    c.bench_function("load jit module in filesystem cache", |b| {
+        b.iter(|| {
+            unsafe { fs_cache.load(&store, key.clone()).unwrap() }
+        })
+    });
+}
+
+pub fn store_cache_native(c: &mut Criterion) {
+    let tmp_dir = TempDir::new("wasmer-cache-bench").unwrap();
+    let mut fs_cache = FileSystemCache::new(tmp_dir.path()).unwrap();
+    let compiler = Singlepass::default();
+    let store = Store::new(&Native::new(compiler).engine());
+    let module = Module::new(&store, std::fs::read("../../lib/c-api/tests/assets/qjs.wasm").unwrap()).unwrap();
+
+    c.bench_function("store native module in filesystem cache", |b| {
+        b.iter(|| {
+            let key = random_key();
+            fs_cache.store(key, &module).unwrap()
+        })
+    });
+}
+
+pub fn load_cache_native(c: &mut Criterion) {
+    let tmp_dir = TempDir::new("wasmer-cache-bench").unwrap();
+    let mut fs_cache = FileSystemCache::new(tmp_dir.path()).unwrap();
+    let compiler = Singlepass::default();
+    let store = Store::new(&Native::new(compiler).engine());
+    let module = Module::new(&store, std::fs::read("../../lib/c-api/tests/assets/qjs.wasm").unwrap()).unwrap();
+    let key = Hash::new([0u8; 32]);
+    fs_cache.store(key, &module).unwrap();
+
+    c.bench_function("load native module in filesystem cache", |b| {
         b.iter(|| {
             unsafe { fs_cache.load(&store, key.clone()).unwrap() }
         })
@@ -46,6 +78,6 @@ pub fn load_cache(c: &mut Criterion) {
 criterion_group! {
     name = benches;
     config = Criterion::default().sample_size(300);
-    targets = store_cache, load_cache
+    targets = store_cache_jit, load_cache_jit, store_cache_native, load_cache_native
 }
 criterion_main!(benches);
