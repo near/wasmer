96fa70f55780965f696f69d360f00bb6746338c0
Simonas Kazlauskas <git@kazlauskas.me>
2022-01-11 20:15:57 +0200

    Remove emscripten-related code

--- lib/emscripten/src/exit.rs
+++
@@ -1,7 +0,0 @@
-use crate::EmEnv;
-
-// __exit
-pub fn exit(_ctx: &EmEnv, value: i32) {
-    debug!("emscripten::exit {}", value);
-    ::std::process::exit(value);
-}
--- lib/emscripten/src/inet.rs
+++
@@ -1,6 +0,0 @@
-use crate::EmEnv;
-
-pub fn addr(_ctx: &EmEnv, _cp: i32) -> i32 {
-    debug!("inet::addr({})", _cp);
-    0
-}
